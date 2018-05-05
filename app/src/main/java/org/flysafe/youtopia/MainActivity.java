d78b7df8c369ac625232a69bec9811b90e2d0da2
VMinhoto <vitor.v.minhoto@gmail.com>
2018-05-05 19:30:45 +0100

    Profile Activity

--- app/src/main/java/org/flysafe/youtopia/MainActivity.java
+++ app/src/main/java/org/flysafe/youtopia/MainActivity.java
@@ -1,27 +1,39 @@
 package org.flysafe.youtopia;
 
+import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Button profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Find the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // Sets the Toolbar to act as the ActionBar for this Activity window.
-        // Make sure the toolbar exists in the activity and is not null
-        setSupportActionBar(toolbar);
-        Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar);
-        TextView mTitle = (TextView) toolbarTop.findViewById(R.id.toolbar_title);
+        profile = (Button) findViewById(R.id.button_profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showProfile();
+            }
+        });
 
     }
+
+    public void showProfile() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+
+    }
+
 }
